8d8b67f48cf1a70874ac6eced4fc3ce54ef44187
rdojeda <ojeda.robertodaniel@gmail.com>
2020-06-28 22:35:53 -0300

    Prueba envio de datos con formulario

--- index.js
+++ index.js
@@ -3,11 +3,20 @@ const express = require('express')
 const server = express()
 
 const public = express.static('public')
+const urlencode = express.urlencoded({ extended: false })
+const json = express.json()
 
-server.use( public )
+server.use( public ) //<--- middleware software que esta en medio de la petición http- y la respuesta del servidor esa petición-
+server.use( urlencode )
+server.use( json )
+
+server.post('/enviar', (req, res) => {
     
-server.post('/enviar', (req, res) =>{
-    res.send('<h1>Hello Form</h1>')
+    const datos = req.body
+    console.log('Estos son los datos enviados.')
+    console.log(datos)
+    
+    res.json({ rta: 'OK'})
 })
 
 
