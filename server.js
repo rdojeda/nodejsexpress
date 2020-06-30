e89dd96f1652ad805374d7541471ef2583c253dd
rdojeda <ojeda.robertodaniel@gmail.com>
2020-06-30 19:02:26 -0300

    Integrando con MongoDb + Cluster

--- server.js
+++ server.js
@@ -1,19 +1,36 @@
 const express = require('express')
+const { MongoClient }  = require('mongodb')
 const server = express()
 
 const urlencoded = express.urlencoded({ extended: true })
 const json = express.json()
 
-const DB = []
 
-console.log( DB )
+//mongodb+srv://Dbojedaweb:rdo0j3d6@cluster0.xqb2m.mongodb.net/Cluster0?retryWrites=true&w=majority
+//en Robo 3T create conexión en Name MongoDb Atlas address cargar  cluster0.xqb2m.mongodb.net mantener port 27017
+
+//MongoClient.paraHacerCosasLocas()
+
+const url = ` mongodb+srv://${process.env.MONGO_DB_USER}:${process.env.MONGO_DB_PASS}@${process.env.MONGO_DB_HOST}/${process.env.MONGO_DB_BASE}?retryWrites=true&w=majority`;
+
+let DB = null
+    
+MongoClient.connect(url, { useUnifiedTopology: true }, function (
+  error,
+  client
+) {
+  DB = client.db("MercadoTech");
+});
+console.log('El servidor de MongoDB es:')
+console.log( process.env.MONGO_DB_HOST )
 
 server.use( json )
 server.use( urlencoded )
 server.listen( 5000 )
 
 server.get('/api', (req, res) => { // Obtener los datos
-    res.json( DB )
+    
+    res.json( DB.collection('Productos').find({}).toArray() )
 }) 
 
 server.post('/api', (req, res) => {  // Crear con datos
@@ -22,8 +39,6 @@ server.post('/api', (req, res) => {  // Crear con datos
         irrepetible
         Autoasignable
     */
-
-
 
     const datos = req.body
     //faltan las validaciones 
@@ -45,3 +60,4 @@ server.put('/api', (req, res) => { // Actualizar con datos
 server.delete('/api', (req, res) => { // Eliminar los datos
     res.json({  rta : 'OK eliminado el producto'  })
 }) 
+
